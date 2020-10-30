0285ec45f1aeb895c240f13d41a268d6bce8bb6a
harshh-mahajan <67816229+harshh-mahajan@users.noreply.github.com>
2020-10-30 12:25:20 +0530

    Update media_preprocessor.py

--- media_preprocessor/media_preprocessor.py
+++ media_preprocessor/media_preprocessor.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 import cv2
 
-class pre_pro:
+class pro_pre:
     def nbg_vc(self,shape):
         shape=shape
         cap = cv2.VideoCapture(0)
